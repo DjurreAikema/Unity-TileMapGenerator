f21f2cb2984ddf3cfeb53b0c227a57fed864bdf6
Djurre <djurre10@hotmail.com>
2019-10-30 23:37:01 +0100

    Splitting up the code

---
+++ Assets/CAMapGen/Models/Coord.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Coord
+{
+    public int tileX;
+    public int tileY;
+
+    public Coord()
+    {
+
+    }
+
+    public Coord(int x, int y)
+    {
+        tileX = x;
+        tileY = y;
+    }
+}
+
